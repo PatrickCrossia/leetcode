7ae2b99edef300c6c61d483b7d9214bfaa33571b
Patrick Crossia <61718317+PatrickCrossia@users.noreply.github.com>
2020-03-02 21:39:54 -0500

    Update attempt_2.py

--- array/1295.find-numbers-with-even-number-of-digits/attempt_2.py
+++ array/1295.find-numbers-with-even-number-of-digits/attempt_2.py
@@ -5,8 +5,7 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        for i in range(len(nums)):
-            x = str(nums[i])
-            if len(x)%2 == 0:
+        for i in nums:
+            if len(str(i)) % 2 == 0:
                 count += 1
         return count
